416816275f9aff24de140a86efc883e250895802
wattemma <wattcemma@gmail.com>
2025-06-10 16:44:12 -0400

    login spec & inventory spec, formatting and tests

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,39 +1,36 @@
-import { test, expect } from "@fixtures/login";
+import { test, expect } from "@fixtures/custom-fixtures";
 import { users } from "@testdata/users";
 
-
 test.describe('@ui @positive', () => {
-  test('QA1 - User can login with valid credentials', async ({ loginPage }) => {
-    await loginPage.page.goto("/");
-    await loginPage.page.fill("#user-name", process.env.USERNAME!);
-    await loginPage.page.fill("#password", process.env.PASSWORD!);
-    await loginPage.page.click("//input[@id='login-button']");
+  test.beforeEach(async ({ loginPage }) => {
+    await loginPage.goto();
+  });
 
+  test('QA1 - User can login with valid credentials', async ({ loginPage }) => {
+    await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
     expect(await loginPage.isLoginSuccessful()).toBeTruthy();
   });
-});
 
-test.describe('@ui @positive', () => {
   test('QA2 - User can login and see inventory page', async ({ loginPage }) => {
-    await loginPage.page.goto("/inventory.html");
+    await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
+    await loginPage.page.waitForURL('/inventory.html');
     expect(await loginPage.isLoginSuccessful()).toBeTruthy();
   });
 });
 
 test.describe('@ui @negative', () => {
-  test('QA3 - User sees error with invalid credentials', async ({ loginPage }) => {
-    await loginPage.page.goto('/');
-    await loginPage.loginWith(users.invalid.username, users.invalid.password);
+  test.beforeEach(async ({ loginPage }) => {
+    await loginPage.goto();
+  });
 
+  test('QA3 - User sees error with invalid credentials', async ({ loginPage }) => {
+    await loginPage.login(users.invalid.username, users.invalid.password);
     const errorMessage = await loginPage.getErrorMessage();
     expect(errorMessage).toContain('Username and password do not match');
   });
-});
-test.describe('@ui @negative', () => {
-  test('QA4 - User sees error with valid username invalid password', async ({ loginPage }) => {
-    await loginPage.page.goto('/');
-    await loginPage.loginWith(process.env.USERNAME!, users.invalid.password);
 
+  test('QA4 - User sees error with valid username and invalid password', async ({ loginPage }) => {
+    await loginPage.login(process.env.USERNAME!, users.invalid.password);
     const errorMessage = await loginPage.getErrorMessage();
     expect(errorMessage).toContain('Username and password do not match');
   });
